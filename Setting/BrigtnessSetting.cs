65196fc73caacf69448743d18ce41d32549bf3d2
SkAlVol <159642986+SkAlVol@users.noreply.github.com>
2024-12-28 13:54:26 +0200

    Update BrigtnessSetting.cs

--- Setting/BrigtnessSetting.cs
+++ Setting/BrigtnessSetting.cs
@@ -3,25 +3,25 @@ using UnityEngine.UI;
 
 public class BrightnessControl : MonoBehaviour
 {
-    public Slider brightnessSlider; // Ссылка на UI Slider
+    public Slider brightnessSlider; 
 
-    // Минимальные и максимальные значения яркости
+    
     public float minAmbientIntensity = 0.2f;
     public float maxAmbientIntensity = 1.0f;
 
     void Start()
     {
-        // Убеждаемся, что есть слайдер
+        
         if (brightnessSlider != null)
         {
-            brightnessSlider.onValueChanged.AddListener(AdjustBrightness); // Добавляем обработчик события
-            brightnessSlider.value = Mathf.InverseLerp(minAmbientIntensity, maxAmbientIntensity, RenderSettings.ambientIntensity); // Инициализация слайдера
+            brightnessSlider.onValueChanged.AddListener(AdjustBrightness); // Г„Г®ГЎГ ГўГ«ГїГҐГ¬ Г®ГЎГ°Г ГЎГ®ГІГ·ГЁГЄ Г±Г®ГЎГ»ГІГЁГї
+            brightnessSlider.value = Mathf.InverseLerp(minAmbientIntensity, maxAmbientIntensity, RenderSettings.ambientIntensity); 
         }
     }
 
     public void AdjustBrightness(float value)
     {
-        // Изменяем интенсивность ambient light в пределах minAmbientIntensity и maxAmbientIntensity
+        
         RenderSettings.ambientIntensity = Mathf.Lerp(minAmbientIntensity, maxAmbientIntensity, value);
     }
 }
