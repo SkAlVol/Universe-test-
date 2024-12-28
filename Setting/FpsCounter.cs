c35482c3577bd5ca4cb8447ca59a21c0276b31fe
SkAlVol <159642986+SkAlVol@users.noreply.github.com>
2024-12-28 13:57:31 +0200

    Кінцевий результат

--- Setting/FpsCounter.cs
+++ Setting/FpsCounter.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class FPSCounter : MonoBehaviour
 {
-    public Text fpsText;  // Ссылка на UI Text для отображения FPS
+    public Text fpsText;  
 
     private void Update()
     {
-        // Рассчитываем FPS
+        
         float currentFPS = 1.0f / Time.unscaledDeltaTime;
 
-        // Обновляем текстовый компонент
+        
         if (fpsText != null)
         {
             fpsText.text = "FPS: " + Mathf.RoundToInt(currentFPS).ToString();
