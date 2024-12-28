05a4ef9bc117b5b37e6ad67b73b8fb6734efa7f8
SkAlVol <159642986+SkAlVol@users.noreply.github.com>
2024-12-28 13:56:43 +0200

    Кінцевий результат

--- Setting/Continue.cs
+++ Setting/Continue.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class CanvasSwitcher : MonoBehaviour
 {
-    public GameObject canvasToHide;       // Ссылка на Canvas, который нужно скрыть
-    public Camera mainCamera;             // Ссылка на основную камеру (Main Camera)
-    public Camera pauseCamera;            // Ссылка на камеру паузы (Pause Camera)
+    public GameObject canvasToHide;       
+    public Camera mainCamera;             
+    public Camera pauseCamera;            
 
-    // Метод, который нужно привязать к кнопке
+    
     public void SwitchToMainCamera()
     {
-        // Проверяем, есть ли Canvas, Pause Camera и Main Camera
+        
         if (canvasToHide != null)
         {
-            canvasToHide.SetActive(false);  // Скрываем Canvas
+            canvasToHide.SetActive(false);  
         }
 
         if (mainCamera != null)
         {
-            mainCamera.gameObject.SetActive(true);  // Включаем Main Camera
+            mainCamera.gameObject.SetActive(true);  
         }
 
         if (pauseCamera != null)
         {
-            pauseCamera.gameObject.SetActive(false);  // Отключаем Pause Camera
+            pauseCamera.gameObject.SetActive(false);  
         }
     }
 }
